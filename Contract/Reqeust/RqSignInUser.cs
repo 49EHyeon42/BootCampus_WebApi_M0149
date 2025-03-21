ac84b254fb7461e0b2d628374d36768aeb748623
DESKTOP-76RGINH\LG <thjo@mostisoft.com>
2025-03-21 22:54:46 +0900

    style: 사용자 접근 요청 설명 추가

--- Contract/Reqeust/RqSignInUser.cs
+++ Contract/Reqeust/RqSignInUser.cs
@@ -4,13 +4,23 @@ namespace Contract.Reqeust
 {
     public class RqSignInUser
     {
+        /// <summary>
+        /// 사용자 이름
+        /// </summary>
         [Required]
         [StringLength(32, MinimumLength = 2, ErrorMessage = "2-32 글자만 가능합니다.")]
         public required string Name { get; init; }
 
+        /// <summary>
+        /// 사용자 성별
+        /// </summary>
         [Required]
         [RegularExpression("^(M|F)$", ErrorMessage = "'M' 또는 'F'만 가능합니다.")]
         public required string Sex { get; init; }
+
+        /// <summary>
+        /// 사용자 생년월일
+        /// </summary>
         [Required]
         // NOTE: custom attribute 필요
         public required DateTime BirthDate { get; init; }
