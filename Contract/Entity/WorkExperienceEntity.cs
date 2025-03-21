bf6f103ff5d39cfabed2fa16f18641cdde525ea5
DESKTOP-76RGINH\LG <thjo@mostisoft.com>
2025-03-21 22:48:39 +0900

    feat: 경력사항 엔티티, Dac 구현

---
+++ Contract/Entity/WorkExperienceEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Contract.Entity
+{
+    public class WorkExperienceEntity : BaseEntity
+    {
+        public required int Id { get; init; }
+        public required int EmployeeId { get; init; }
+        public required DateTime HireDate { get; init; }
+        public DateTime? LeaveDate { get; init; }
+        public string? Description { get; init; }
+    }
+}
---
+++ Dac/WorkExperienceDac.cs
@@ -0,0 +1,78 @@
+﻿using Contract.Entity;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Dac
+{
+    public class WorkExperienceDac
+    {
+        public int InsertWorkExperience(SqlConnection sqlConnection, SqlTransaction sqlTransaction,
+            int employeeId, DateTime hireDate, DateTime? leaveDate, string? description)
+        {
+            string query = @"
+                INSERT INTO WorkExperience (EmployeeId, HireDate, LeaveDate, Description)
+                OUTPUT INSERTED.Id
+                VALUES (@EmployeeId, @HireDate, @LeaveDate, @Description);
+            ";
+
+            int id = sqlConnection.QuerySingle<int>(query, new
+            {
+                EmployeeId = employeeId,
+                HireDate = hireDate,
+                LeaveDate = leaveDate,
+                Description = description
+            }, sqlTransaction);
+
+            return id;
+        }
+
+        public IEnumerable<WorkExperienceEntity> SelectWorkExperienceByEmployeeId(SqlConnection sqlConnection,
+            int employeeId)
+        {
+            string query = @"
+                SELECT *
+                FROM WorkExperience
+                WHERE EmployeeId = @EmployeeId;
+            ";
+
+            return sqlConnection.Query<WorkExperienceEntity>(query, new
+            {
+                EmployeeId = employeeId
+            }).AsList();
+        }
+
+        public void UpdateWorkExperienceById(SqlConnection sqlConnection, SqlTransaction sqlTransaction,
+            int id, DateTime hireDate, DateTime? leaveDate, string? description)
+        {
+            string query = @"
+                UPDATE WorkExperience
+                SET HireDate = @HireDate, LeaveDate = @LeaveDate,
+                    Description = @Description, LastModifiedDate = @LastModifiedDate
+                WHERE Id = @Id;
+            ";
+
+            sqlConnection.Execute(query, new
+            {
+                Id = id,
+                HireDate = hireDate,
+                LeaveDate = leaveDate,
+                Description = description,
+                LastModifiedDate = DateTime.Now
+            }, sqlTransaction);
+        }
+
+        public void DeleteWorkExperienceByEmployeeId(SqlConnection sqlConnection, SqlTransaction sqlTransaction,
+            int employeeId)
+        {
+            string query = @"
+                DELETE FROM WorkExperience
+                WHERE EmployeeId = @EmployeeId;
+            ";
+
+            sqlConnection.Execute(query, new
+            {
+                EmployeeId = employeeId
+            }, sqlTransaction);
+        }
+    }
+}
