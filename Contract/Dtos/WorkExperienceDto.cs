dc0c86dd6e7f495ec0f3847ff31d1110b06def27
DESKTOP-76RGINH\LG <thjo@mostisoft.com>
2025-03-21 22:53:01 +0900

    style: 경력사항 DTO 설명 추가

--- Contract/Dtos/WorkExperienceDto.cs
+++ Contract/Dtos/WorkExperienceDto.cs
@@ -2,10 +2,25 @@
 {
     public class WorkExperienceDto : BaseDtos
     {
+        /// <summary>
+        /// 경력사항 식별자
+        /// </summary>
         public required int Id { get; init; }
+        /// <summary>
+        /// 직원 식별자
+        /// </summary>
         public required int EmployeeId { get; init; }
+        /// <summary>
+        /// 경력사항 입사일
+        /// </summary>
         public required DateTime HireDate { get; init; }
+        /// <summary>
+        /// 경력사항 퇴사일
+        /// </summary>
         public DateTime? LeaveDate { get; init; }
+        /// <summary>
+        /// 경력사항 설명
+        /// </summary>
         public string? Description { get; init; }
     }
 }
