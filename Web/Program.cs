85e542fc3b3ccc6714ebf8ecd03addbc189103c2
DESKTOP-76RGINH\LG <thjo@mostisoft.com>
2025-03-24 10:19:18 +0900

    feat: Serilog 설정

--- Web/Program.cs
+++ Web/Program.cs
@@ -3,10 +3,23 @@ using Biz.Services;
 using Contract.Biz;
 using Contract.Dac;
 using Dac.Daos;
+using Serilog;
 using Web;
 using Web.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.File(
+        path: "Logs/log-.txt",
+        rollingInterval: RollingInterval.Day,
+        outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
+    )
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddSingleton<UserStorage>();
 
